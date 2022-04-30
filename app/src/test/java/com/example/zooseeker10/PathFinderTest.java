69cab65078d8c20b9079453f2d04b37a8c0233dd
Zenas Zhu <zenaszhu02@gmail.com>
2022-04-30 13:53:02 -0700

    more tests and more bad testing framework

--- app/src/test/java/com/example/zooseeker10/PathFinderTest.java
+++ app/src/test/java/com/example/zooseeker10/PathFinderTest.java
@@ -20,7 +20,6 @@ public class PathFinderTest {
     private static final double DOUBLE_EPSILON = 1e-15;
 
     private static PathFinder pathfinder;
-    private static List<String> verticesToVisit;
 
     /**
      * Imports the graph topology from the example file
@@ -39,22 +38,80 @@ public class PathFinderTest {
         ArrayList<GraphPath<String, IdentifiedWeightedEdge>> pathArr = new ArrayList<>(path);
         HashSet<String> vertices = new HashSet<>(toVisit);
         for (int i = 0; i < pathArr.size() - 1; i++) {
-            String exhibit = pathArr.get(i).getStartVertex();
-            assertEquals(tag + ": path disconnected", exhibit, pathArr.get(i+1).getEndVertex());
-            assertFalse(tag + ": duplicated exhibit", vertices.contains(exhibit));
+            String exhibit = pathArr.get(i).getEndVertex();
+            assertEquals(tag + ": path disconnected", exhibit, pathArr.get(i + 1).getStartVertex());
+            assertTrue(tag + ": duplicated exhibit", vertices.contains(exhibit));
             vertices.remove(exhibit);
         }
         assertEquals(tag + ": wrong start", START_VERTEX_ID, pathArr.get(0).getStartVertex());
         assertEquals(tag + ": wrong end", END_VERTEX_ID, pathArr.get(pathArr.size() - 1).getEndVertex());
         double actualWeight = 0;
-        for (GraphPath<String, IdentifiedWeightedEdge> subPath : path) { actualWeight += subPath.getWeight(); };
+        for (GraphPath<String, IdentifiedWeightedEdge> subPath : path) {
+            actualWeight += subPath.getWeight();
+        }
+        ;
         assertTrue(String.format("%s: weight too large (got %f, required %f)", tag, actualWeight, maxWeight), actualWeight <= maxWeight);
+    }
+
+    class SimpleTester {
+        String tag;
+        List<String> verticesToVisit;
+        double maxWeight;
+
+        public SimpleTester(String tag) {
+            this.tag = tag;
+            verticesToVisit = new ArrayList<>();
+        }
+
+        public SimpleTester addDestination(String dest) {
+            verticesToVisit.add(dest);
+            return this;
+        }
+
+        public SimpleTester setMaxWeight(double maxWeight) {
+            this.maxWeight = maxWeight;
+            return this;
+        }
+
+        public void run() {
+            List<GraphPath<String, IdentifiedWeightedEdge>> calculatedPath = pathfinder.findPath(verticesToVisit);
+            checkPathSanity(tag, verticesToVisit, calculatedPath, maxWeight);
+        }
     }
 
     @Test
     public void findPath_noTargets() {
-        verticesToVisit = new ArrayList<>();
-        List<GraphPath<String, IdentifiedWeightedEdge>> calculatedPath = pathfinder.findPath(verticesToVisit);
-        checkPathSanity("no targets", verticesToVisit, calculatedPath, 0);
+        new SimpleTester("no targets")
+                .setMaxWeight(0)
+                .run();
+    }
+
+    @Test
+    public void findPath_oneTarget() {
+        new SimpleTester("one target")
+                .addDestination("gorillas")
+                .setMaxWeight(1000)
+                .run();
+    }
+
+    @Test
+    public void findPath_twoTargets() {
+        new SimpleTester("two targets")
+                .addDestination("gorillas")
+                .addDestination("arctic_foxes")
+                .setMaxWeight(1600)
+                .run();
+    }
+
+    @Test
+    public void findPath_allTargets() {
+        new SimpleTester("all targets")
+                .addDestination("arctic_foxes")
+                .addDestination("elephant_odyssey")
+                .addDestination("gators")
+                .addDestination("gorillas")
+                .addDestination("lions")
+                .setMaxWeight(2400)
+                .run();
     }
 }
