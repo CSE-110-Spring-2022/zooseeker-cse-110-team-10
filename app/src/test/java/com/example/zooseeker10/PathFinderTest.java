fb2d739b3b4a9fb308647fd9ed492273eb44c570
Zenas Zhu <zenaszhu02@gmail.com>
2022-04-30 12:51:01 -0700

    untested PathFinder testing helper

--- app/src/test/java/com/example/zooseeker10/PathFinderTest.java
+++ app/src/test/java/com/example/zooseeker10/PathFinderTest.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class PathFinderTest {
@@ -33,17 +34,27 @@ public class PathFinderTest {
         pathfinder = new PathFinder(graph, START_VERTEX_ID, END_VERTEX_ID);
     }
 
+    public void checkPathSanity(String tag, List<String> toVisit, List<GraphPath<String, IdentifiedWeightedEdge>> path, double maxWeight) {
+        assertEquals(tag + ": wrong number of subpaths", toVisit.size() + 1, path.size());
+        ArrayList<GraphPath<String, IdentifiedWeightedEdge>> pathArr = new ArrayList<>(path);
+        HashSet<String> vertices = new HashSet<>(toVisit);
+        for (int i = 0; i < pathArr.size() - 1; i++) {
+            String exhibit = pathArr.get(i).getStartVertex();
+            assertEquals(tag + ": path disconnected", exhibit, pathArr.get(i+1).getEndVertex());
+            assertFalse(tag + ": duplicated exhibit", vertices.contains(exhibit));
+            vertices.remove(exhibit);
+        }
+        assertEquals(tag + ": wrong start", START_VERTEX_ID, pathArr.get(0).getStartVertex());
+        assertEquals(tag + ": wrong end", END_VERTEX_ID, pathArr.get(pathArr.size() - 1).getEndVertex());
+        double actualWeight = 0;
+        for (GraphPath<String, IdentifiedWeightedEdge> subPath : path) { actualWeight += subPath.getWeight(); };
+        assertTrue(String.format("%s: weight too large (got %f, required %f)", tag, actualWeight, maxWeight), actualWeight <= maxWeight);
+    }
+
     @Test
     public void findPath_noTargets() {
         verticesToVisit = new ArrayList<>();
         List<GraphPath<String, IdentifiedWeightedEdge>> calculatedPath = pathfinder.findPath(verticesToVisit);
-
-        assertEquals("Number of subpaths incorrect", 1, calculatedPath.size());
-
-        GraphPath<String, IdentifiedWeightedEdge> path = calculatedPath.get(0);
-        assertEquals("Weight of subpath incorrect", 0, path.getWeight(), DOUBLE_EPSILON);
-        assertEquals("Number of vertices along subpath incorrect", 0, path.getLength());
-        assertEquals("Number of vertices in subpath incorrect", 1, path.getVertexList().size());
-        assertEquals("Vertex in subpath incorrect", START_VERTEX_ID, path.getStartVertex());
+        checkPathSanity("no targets", verticesToVisit, calculatedPath, 0);
     }
 }
