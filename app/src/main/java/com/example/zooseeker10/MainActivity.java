2cc45f18e1d987f4c1f8f828e1d16f26931afaf1
Zenas Zhu <zenaszhu02@gmail.com>
2022-05-01 18:58:38 -0700

    plan button yay

--- app/src/main/java/com/example/zooseeker10/MainActivity.java
+++ app/src/main/java/com/example/zooseeker10/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -16,11 +17,13 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
     private static final int SECOND_ACTIVITY_REQUEST_CODE = 0;
     ArrayList<String> selectedExhibits = new ArrayList<>();
+    Button planButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        planButton = this.findViewById(R.id.plan_btn);
 
 
 //        Map<String, ZooData.VertexInfo> vertexInfoMap = ZooData.loadVertexInfoJSON(this, "sample_node_info.json");
@@ -31,14 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    int i = 0; // TODO:this is hack. obviously
     public void onSearchButtonClicked(View view) {
-        i++;
-        if (i == 4) {
-            Intent intent = new Intent(this, PlanActivity.class);
-            intent.putStringArrayListExtra("exhibits", selectedExhibits);
-            startActivity(intent);
-        }
         EditText searchBarView = findViewById(R.id.search_bar_view);
         String searchQuery = searchBarView.getText().toString();
 
@@ -50,14 +46,25 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void onPlanButtonClicked(View view) {
+        Intent intent = new Intent(this, PlanActivity.class);
+        intent.putStringArrayListExtra("exhibits", selectedExhibits);
+        startActivity(intent);
+    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == SECOND_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 String exhibitId = data.getStringExtra("exhibitId");
-                selectedExhibits.add(exhibitId);
-                Log.d("MainActivity", exhibitId);
+                if (selectedExhibits.isEmpty()) {
+                    planButton.setVisibility(View.VISIBLE);
+                }
+                if (!selectedExhibits.contains(exhibitId)) {
+                    selectedExhibits.add(exhibitId);
+                    Log.d("MainActivity", exhibitId);
+                }
             }
         }
     }
