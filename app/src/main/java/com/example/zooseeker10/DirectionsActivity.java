06ce4023267c6d118e0f7ab706f655a6293f372e
Zenas Zhu <zenaszhu02@gmail.com>
2022-05-04 16:19:07 -0700

    Convert path into human-readable directions

--- app/src/main/java/com/example/zooseeker10/DirectionsActivity.java
+++ app/src/main/java/com/example/zooseeker10/DirectionsActivity.java
@@ -6,6 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.jgrapht.Graph;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 public class DirectionsActivity extends AppCompatActivity {
 
     @Override
@@ -13,6 +24,37 @@ public class DirectionsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_directions);
         Intent intent = getIntent();
-        Log.d("DirectionsActivity", intent.getStringExtra("paths"));
+        Gson gson = new Gson();
+        Type pathIDsType = new TypeToken<List<List<String>>>() {}.getType();
+        List<List<String>> paths = gson.fromJson(intent.getStringExtra("paths"), pathIDsType);
+        for (List<String> path : paths) {
+            explainPath(path);
+        }
+    }
+
+    public List<String> explainPath(List<String> path) {
+        Map<String, ZooData.VertexInfo> vertexInfo = ZooData.loadVertexInfoJSON(this, ZooData.NODE_INFO_PATH);
+        Map<String, ZooData.EdgeInfo> edgeInfo = ZooData.loadEdgeInfoJSON(this, ZooData.EDGE_INFO_PATH);
+        Graph<String, IdentifiedWeightedEdge> g = ZooData.loadZooGraphJSON(this, ZooData.ZOO_GRAPH_PATH);
+        Log.d("DirectionsActivity", "directions to: " + path.get(path.size() - 1));
+        List<String> explains = new ArrayList<>();
+        Iterator<String> parts = path.iterator();
+        String lastVertex = parts.next();
+        while (parts.hasNext()) {
+            String nextEdge = parts.next();
+            String nextVertex = parts.next();
+            String explain =
+                    String.format("%s ft along %s from %s to %s",
+                            g.getEdgeWeight(g.getEdge(lastVertex, nextVertex)),
+                            edgeInfo.get(nextEdge).street,
+                            vertexInfo.get(lastVertex).name,
+                            vertexInfo.get(nextVertex).name
+
+                    );
+            explains.add(explain);
+            Log.d("DirectionsActivity", explain);
+            lastVertex = nextVertex;
+        }
+        return explains;
     }
 }
