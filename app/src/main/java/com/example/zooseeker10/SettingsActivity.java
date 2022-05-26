716c6d5a6cd4a7485252a5a9615caa0559a92ffe
Derick <djang@ucsd.edu>
2022-05-26 14:09:26 -0700

    "Completed: added generate brief/detailed directions, ui for settings activity, switch between brief/detailed directions In Progress: Saving state of settings activity"

--- app/src/main/java/com/example/zooseeker10/SettingsActivity.java
+++ app/src/main/java/com/example/zooseeker10/SettingsActivity.java
@@ -4,20 +4,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
-    private boolean directionsType;
+    private boolean directionsType=false;
+    private Switch toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-    }
 
-    public void onDirectionsTypeSwitchClicked(View view){
-        directionsType=true;
+        toggle=findViewById(R.id.directions_type_switch);
+
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked){
+                    directionsType=true;
+                    Log.d("Directions Type", "True");
+                }
+                else{
+                    directionsType=false;
+                    Log.d("Directions Type", "False");
+                }
+            }
+        });
     }
 
     public void onBackButtonClicked(View view){
