10621c4e2d318fae2c3a3cb1e8cc606d6bb77876
Alex Simonyan <alex.simonyan2002@gmail.com>
2022-05-31 00:39:34 -0700

    added location mocking, handling changed locations

--- app/src/main/java/com/example/zooseeker10/DirectionsActivity.java
+++ app/src/main/java/com/example/zooseeker10/DirectionsActivity.java
@@ -1,11 +1,7 @@
 package com.example.zooseeker10;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
@@ -14,12 +10,19 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.maps.model.LatLng;
 
-import org.jgrapht.Graph;
 import org.jgrapht.GraphPath;
 
 import java.util.List;
@@ -28,16 +31,18 @@ import java.util.Map;
 public class DirectionsActivity extends AppCompatActivity {
 
     public static boolean callReplan;
-    Button previousButton;
-    Button nextButton;
-    TextView directionsTitle;
+    private static final boolean listenToGPS = false;
+    private Button previousButton;
+    private Button nextButton;
+    private TextView directionsTitle;
 
-    ZooPlan plan;
-    ZooPlan.ZooWalker walker;
-    DirectionsListAdapter dLAdapter;
-    Map<String, ZooData.VertexInfo> vertexInfo;
-    PathFinder pf;
-    String lastVertexLocation;
+    private ZooPlan plan;
+    private ZooPlan.ZooWalker walker;
+    private DirectionsListAdapter dLAdapter;
+    private Map<String, ZooData.VertexInfo> vertexInfo;
+    private PathFinder pf;
+    private String lastVertexLocation;
+    private UserTracker userTracker;
 
     enum Directions {
         FORWARD,
@@ -61,6 +66,9 @@ public class DirectionsActivity extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.directions_list);
         directionsTitle = findViewById(R.id.directions_title);
 
+        var mockButton = findViewById(R.id.mock_btn);
+        mockButton.setOnClickListener(this::onMockButtonClicked);
+
         // Setup for views with text
         dLAdapter = new DirectionsListAdapter();
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -68,49 +76,14 @@ public class DirectionsActivity extends AppCompatActivity {
 
         // Loads up initial page
         setDirectionsPage(Directions.BACKWARD);
-        UserTracker userTracker = new UserTracker(plan, walker);
+        userTracker = new UserTracker(plan, walker);
 
-        /* Listen for Location Updates */
-        {
-            var provider = LocationManager.GPS_PROVIDER;
-            var locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-
-            var locationListener = new LocationListener() {
-                @Override
-                public void onLocationChanged(@NonNull Location location) {
-                    Log.d("DirectionsActivity", String.format("Location changed: %s", location));
-
-                    LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
-                    userTracker.setUserLocation(currentLocation);
-                    lastVertexLocation = userTracker.getClosestVertex().id;
-                    if (userTracker.isOffTrack()) {
-                        recalculatePath();
-                    }
-                }
-            };
-            locationManager.requestLocationUpdates(provider, 0, 0f, locationListener);
-
-            Location location = locationManager.getLastKnownLocation(provider);
-            LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
-            userTracker.setUserLocation(currentLatLng);
-            lastVertexLocation = userTracker.getClosestVertex().id;
-            if (userTracker.isOffTrack()) {
-                recalculatePath();
-            }
+        if (listenToGPS) {
+            setupLocationListener();
         }
 
-        previousButton.setOnClickListener(
-                view -> {
-                    setDirectionsPage(Directions.BACKWARD);
-                }
-        );
-
-        nextButton.setOnClickListener(
-                view -> {
-                    setDirectionsPage(Directions.FORWARD);
-                }
-        );
-
+        previousButton.setOnClickListener(view -> setDirectionsPage(Directions.BACKWARD));
+        nextButton.setOnClickListener(view -> setDirectionsPage(Directions.FORWARD));
     }
 
     public void recalculatePath() {
@@ -139,9 +112,74 @@ public class DirectionsActivity extends AppCompatActivity {
 
         List<DirectionsItem> displayedDirections = walker.explainPath(this);
         dLAdapter.setDirectionsItems(displayedDirections);
-        directionsTitle.setText(String.format("Directions from %s to %s",
-                displayedDirections.get(0).from,
+        directionsTitle.setText(String.format("Directions to %s",
                 displayedDirections.get(displayedDirections.size() - 1).to
         ));
     }
+
+    @SuppressLint("MissingPermission")
+    private void setupLocationListener() {
+        var provider = LocationManager.GPS_PROVIDER;
+        var locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        var locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                handleLocationChanged(new LatLng(location.getLatitude(), location.getLongitude()));
+            }
+        };
+        locationManager.requestLocationUpdates(provider, 0, 0f, locationListener);
+    }
+
+    private void handleLocationChanged(@NonNull LatLng currentLocation) {
+        Log.d("DirectionsActivity", String.format("Location changed: %s", currentLocation));
+
+        userTracker.setUserLocation(currentLocation);
+        lastVertexLocation = userTracker.getClosestVertex().id;
+        if (userTracker.isOffTrack()) {
+            recalculatePath();
+        }
+        if (userTracker.needsReplan()) {
+            new ReplanPrompt(this).showPrompt();
+        }
+        dLAdapter.setDirectionsItems(walker.explainPath(this));
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void onMockButtonClicked(View view) {
+        // TODO: could define this layout in an XML and inflate it, instead of defining in code...
+        var inputType = EditorInfo.TYPE_CLASS_NUMBER
+            | EditorInfo.TYPE_NUMBER_FLAG_SIGNED
+            | EditorInfo.TYPE_NUMBER_FLAG_DECIMAL;
+
+        final EditText latInput = new EditText(this);
+        latInput.setInputType(inputType);
+        latInput.setHint("Latitude");
+        latInput.setText("32.737986");
+
+        final EditText lngInput = new EditText(this);
+        lngInput.setInputType(inputType);
+        lngInput.setHint("Longitude");
+        lngInput.setText("-117.169499");
+
+        final LinearLayout layout = new LinearLayout(this);
+        layout.setDividerPadding(8);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.addView(latInput);
+        layout.addView(lngInput);
+
+        var builder = new AlertDialog.Builder(this)
+            .setTitle("Inject a Mock Location")
+            .setView(layout)
+            .setPositiveButton("Submit", (dialog, which) -> {
+                var lat = Double.parseDouble(latInput.getText().toString());
+                var lng = Double.parseDouble(lngInput.getText().toString());
+                handleLocationChanged(new LatLng(lat, lng));
+            })
+            .setNegativeButton("Cancel", (dialog, which) -> {
+                dialog.cancel();
+            });
+        builder.show();
+    }
+
 }
